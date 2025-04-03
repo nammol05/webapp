8dd81dbebd56aa43ade40777010de906e8f7dc58
Nammon <nammol05@gmail.com>
2025-04-03 10:28:41 +0700

    Added texts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
-  return (
+  return (<div>
     <div>Hello world!</div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+      Dolor praesentium fugit nesciunt quos quibusdam laudantium earum quaerat pariatur alias sit.</p>
+    </div>
   );
 }
